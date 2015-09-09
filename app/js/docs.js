40e8e841d3d46530a9243671c89d441a0899caff
devoluo <lgclgclgc234@163.com>
2015-09-09 13:44:55 +0800

    加入注释

--- app/js/docs.js
+++ app/js/docs.js
@@ -3,79 +3,32 @@
 
   /* code here! */
 
-  // function falls(cantainer){
-  //   var f = inherit(falls.methods);
-  //   f.container = document.getElementById(container);
-  //   return f;
-  // }
-
-  // function extends(Class){
-  //   var fn = function(){};
-  //   fn.prototype = new Class();
-  //   return fn;
-  // }
 
   // window.console.log = function(){};
 
-
-  function Falls(){
-  	this.container = document.getElementById('falls');
+  /**
+   * 瀑布流类
+   * @param {[type]} container [容器ID]
+   */
+  function Falls(container){
+  	this.container = document.getElementById(container);
   	this.boxs = this.container.children;
-  	this.IMGWIDTH = this.boxs[0].offsetWidth;
-  	this.columns = Math.floor(document.documentElement.clientWidth / this.IMGWIDTH);
+  	this.colWidth = this.boxs[0].offsetWidth;
+  	this.columns = Math.floor(document.documentElement.clientWidth / this.colWidth);
   	this.columnsHeight = [];
   	this.lastsortImg = this.columns;
-  	// this.setContainerWidth=function(){
-  	// 	this.container.style.width = this.IMGWIDTH * this.columns + "px";
-  	// };
-  	// this.getColumnsHeight=function(){
-	  // 	for(var i = 0;i < this.columns;i ++){
-	  // 		this.columnsHeight[i] = this.boxs[i].offsetHeight;
-	  // 		console.log(this.boxs[i].offsetHeight);
-	  // 	}
-	  // 	console.log(this.columnsHeight);
-	  // };
-  	// this.getMinHeightIndex=function(columnsHeight,minColHeight){
-	  // 	for(var i in columnsHeight){
-	  // 		if(columnsHeight[i] === minColHeight){
-	  // 			return i;
-	  // 		}
-	  // 	}
-	  // };
-  	// this.sortIMG=function(){
-	  // 	for(var i = this.lastsortImg + 1;i < this.boxs.length; i ++){
-	  // 		var minColHeight = Math.min.apply(null,this.columnsHeight);
-	  // 		console.log(minColHeight);
-	  // 		var minColIndex = this.getMinHeightIndex(this.columnsHeight,minColHeight);
-	  // 		console.log(minColIndex);
-	  // 		var minColWidth = minColIndex * this.IMGWIDTH;
-	  // 		this.boxs[i].style.position = "absolute";
-	  // 		this.boxs[i].style.top = minColHeight + "px";
-	  // 		this.boxs[i].style.left = minColWidth + "px";
-	  // 		this.columnsHeight[minColIndex] += this.boxs[i].offsetHeight;
-	  // 		console.log(this.columnsHeight[minColIndex]);
-	  // 		this.lastsortImg = i;
-	  // 	}
-	  // };
-   //  this.checkScroll = function(){
-   //    var lastTop = this.boxs[this.boxs.length - 1].offsetTop;
-   //    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-   //    var clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
-   //    if(lastTop < scrollTop + clientHeight){
-   //      return true;
-   //    }
-   //  };
-   //  this.init = function(){
-   //    this.setContainerWidth();
-   //    this.getColumnsHeight();
-   //    this.sortIMG();
-   //  };
   }
 
+  /**
+   * 瀑布流原型链
+   * @type {Object}
+   */
   Falls.prototype = {
+    //设置容器宽度，根据列和列宽
     setContainerWidth:function(){
-      this.container.style.width = this.IMGWIDTH * this.columns + "px";
+      this.container.style.width = this.colWidth * this.columns + "px";
     },
+    //获得初始化时候的列的高度
     getColumnsHeight:function(){
       for(var i = 0;i < this.columns;i ++){
         this.columnsHeight[i] = this.boxs[i].offsetHeight;
@@ -83,6 +36,7 @@
       }
       console.log(this.columnsHeight);
     },
+    //得到列的最低高度，将图片塞到最低高度的下面
     getMinHeightIndex:function(columnsHeight,minColHeight){
       for(var i in columnsHeight){
         if(columnsHeight[i] === minColHeight){
@@ -90,21 +44,23 @@
         }
       }
     },
+    //将图片塞到最低高度的下面
     sortIMG:function(){
       for(var i = this.lastsortImg + 1;i < this.boxs.length; i ++){
         var minColHeight = Math.min.apply(null,this.columnsHeight);
         console.log(minColHeight);
         var minColIndex = this.getMinHeightIndex(this.columnsHeight,minColHeight);
         console.log(minColIndex);
-        var minColWidth = minColIndex * this.IMGWIDTH;
+        var minColLeft = minColIndex * this.colWidth;
         this.boxs[i].style.position = "absolute";
         this.boxs[i].style.top = minColHeight + "px";
-        this.boxs[i].style.left = minColWidth + "px";
+        this.boxs[i].style.left = minColLeft + "px";
         this.columnsHeight[minColIndex] += this.boxs[i].offsetHeight;
         console.log(this.columnsHeight[minColIndex]);
         this.lastsortImg = i;
       }
     },
+    //检查是否加载新图
     checkScroll:function(){
       var lastTop = this.boxs[this.boxs.length - 1].offsetTop;
       var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
@@ -113,6 +69,7 @@
         return true;
       }
     },
+    //初始化调用
     init:function(){
       this.setContainerWidth();
       this.getColumnsHeight();
@@ -121,7 +78,7 @@
 
   }
 
-  var falls = new Falls();
+  var falls = new Falls("falls");
   var tpl = tpl || document.getElementById('tpl').innerHTML;
   document.getElementById('tpl').remove();
   var html = _.template(tpl);
@@ -139,83 +96,24 @@
   ];
   var list={
     data: data
-  }
+  };
 
-
-
-
-
+  /**
+   * 在onload事件中监听，否则，图片位置会乱
+   * @return {[type]} [description]
+   */
   window.onload = function(){   
     falls.init();
   }
-
+  /**
+   * 在onscroll中监听是否加载
+   * @return {[type]} [description]
+   */
   window.onscroll = function(){
     if(falls.checkScroll()){
        falls.container.innerHTML += html(list);
        falls.sortIMG();
     }
   }
-
-  
-
-
-  // var container = document.getElementById('falls');
-  // var boxs = container.children;
-  // var IMGWIDTH = boxs[0].offsetWidth;
-
-  // var getColumns = function(){
-  // 	var screenWidth = document.documentElement.clientWidth;
-  // 	return Math.floor(screenWidth / IMGWIDTH);
-  // };
-
-  // var columns = getColumns();
-  // console.log(columns);
-
-  // var setContainerWidth = function(container,columns){
-  // 	container.style.width = IMGWIDTH * columns + "px";
-  // };
-
-  // setContainerWidth(container,columns);
-
-  // var columnsHeight = [];
-
-  // var getColumnsHeight = function(columns,boxs){
-  // 	for(var i = 0;i < columns;i ++){
-  // 		columnsHeight[i] = boxs[i].offsetHeight;
-  // 		console.log(boxs[i].offsetHeight);
-  // 		console.log(document.getElementById('falls').children[i].offsetHeight);
-  // 	}
-  // };
-
-  // getColumnsHeight(columns,boxs);
-  // console.log(columnsHeight)
-
-  // var lastsortImg = columns;
-
-  // var getMinHeightIndex = function(columnsHeight,minColHeight){
-  // 	for(var i in columnsHeight){
-  // 		if(columnsHeight[i] === minColHeight){
-  // 			return i;
-  // 		}
-  // 	}
-  // }
-
-  // var sortIMG = function(boxs){
-  // 	for(var i = lastsortImg;i < boxs.length; i ++){
-  // 		var minColHeight = Math.min.apply(null,columnsHeight);
-  // 		console.log(minColHeight);
-  // 		var minColIndex = getMinHeightIndex(columnsHeight,minColHeight);
-  // 		console.log(minColIndex);
-  // 		var minColWidth = minColIndex * IMGWIDTH;
-  // 		boxs[i].style.position = "absolute";
-  // 		boxs[i].style.top = minColHeight + "px";
-  // 		boxs[i].style.left = minColWidth + "px";
-  // 		columnsHeight[minColIndex] += boxs[i].offsetHeight;
-  // 		console.log(columnsHeight[minColIndex]);
-  // 		lastsortImg = i;
-  // 	}
-  // }
-
-  // sortIMG(boxs);
 
 })();
